48e6dde7f5827ddcf2eeba56c757687178955cf0
Josh <jkrowden@gmail.com>
2022-07-26 14:50:01 -0600

    Attempt at animations for content box

--- src/Components/Dashboard/Content/Content.jsx
+++ src/Components/Dashboard/Content/Content.jsx
@@ -1,23 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { Row, Col } from "react-bootstrap";
-
 
 import { Home, Code, Games, Socials } from "../Content";
 
-import { Routes, Route} from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 
-class Content extends Component {
-    state = {  } 
-    render() { 
-        return (<div className="content">
-            <Routes>
-                <Route path="/" element={<Home/>}></Route>
-                <Route path="/code" element={<Code/>}></Route>
-                <Route path="/games" element={<Games/>}></Route>
-                <Route path="/socials" element={<Socials/>}></Route>
-            </Routes>
-        </div>);
-    }
+import "./Content.css"
+
+function Content() {
+
+    var location = useLocation();
+    return (
+        <div className="content">
+          <TransitionGroup>
+            <CSSTransition key={location.key} classNames="slide" timeout={1000}>
+              <Routes>
+                <Route path="/" element={<Home />} exact></Route>
+                <Route path="/code" element={<Code />}></Route>
+                <Route path="/games" element={<Games />}></Route>
+                <Route path="/socials" element={<Socials />}></Route>
+              </Routes>
+            </CSSTransition>
+          </TransitionGroup>
+        </div>
+      );
 }
- 
+
 export default Content;
